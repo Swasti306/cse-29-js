3fd5c27a078a1aaf7cff921da2b62ac258aa67f6
Swasti306 <swasti.25b01010383@abes.ac.in>
2026-03-18 17:15:40 +0530

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,18 @@
+// const x=document.getElementById("p1");
+// x.innerHTML="swasti singh";
+
+// const result=document.getElementById("para");
+// result.innerHTML="my self";
+// result.title="myname";
+
+// const result=document.getElementById("p1");
+// console.log(result);
+
+
+// result[1].innerhtml="singh";
+// result[0].innerhtml="swasti";
+
+// const x=document.getElementById("ss").getAttribute("class");
+// document.getElementById("d").innerHTML=x;
+
+var text ='{"employees":{'+'{"firstname":"john","lastname":Dor"},'+'{"firstname":Anna","lastname":"smith"},'
